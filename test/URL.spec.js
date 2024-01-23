a838d0eee33da10316c13fac42c8e0ed71ef5cc0
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-01-23 09:21:31 -0800

    test: add blessed protocol urls

--- test/URL.spec.js
+++ test/URL.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'aegir/chai';
 
 describe('URL-test', () => {
-  it(`works in ${process.env.RUNNER_ENV}`, () => {
+  it(`ipfs:// URLs work in ${process.env.RUNNER_ENV}`, () => {
     const url = new URL('ipfs://bafyFoo');
     if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
       expect(url.protocol).to.equal('ipfs:');
@@ -19,5 +19,46 @@ describe('URL-test', () => {
       expect(url.host).to.equal('bafyfoo');
       expect(url.hostname).to.equal('bafyfoo');
     }
+  });
+  it(`ipns:// URLs work in ${process.env.RUNNER_ENV}`, () => {
+    const url = new URL('ipns://bafyFoo');
+    if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
+      expect(url.protocol).to.equal('ipns:');
+      expect(url.pathname).to.equal('');
+      expect(url.host).to.equal('bafyFoo');
+      expect(url.hostname).to.equal('bafyFoo');
+    } else if (process.env.RUNNER_ENV.includes('chrome')) {
+      expect(url.protocol).to.equal('ipns:');
+      expect(url.pathname).to.equal('//bafyFoo');
+      expect(url.host).to.equal('');
+      expect(url.hostname).to.equal('');
+    } else if (process.env.RUNNER_ENV.includes('firefox')) {
+      expect(url.protocol).to.equal('ipns:');
+      expect(url.pathname).to.equal('/');
+      expect(url.host).to.equal('bafyfoo');
+      expect(url.hostname).to.equal('bafyfoo');
+    }
+  });
+
+  it(`http:// URLs work in ${process.env.RUNNER_ENV}`, () => {
+    const url = new URL('http://bafyFoo.com');
+    expect(url.protocol).to.equal('http:');
+    expect(url.pathname).to.equal('/');
+    expect(url.host).to.equal('bafyfoo.com');
+    expect(url.hostname).to.equal('bafyfoo.com');
+  });
+  it(`https:// URLs work in ${process.env.RUNNER_ENV}`, () => {
+    const url = new URL('https://bafyFoo.com');
+    expect(url.protocol).to.equal('https:');
+    expect(url.pathname).to.equal('/');
+    expect(url.host).to.equal('bafyfoo.com');
+    expect(url.hostname).to.equal('bafyfoo.com');
+  });
+  it(`ftp:// URLs work in ${process.env.RUNNER_ENV}`, () => {
+    const url = new URL('ftp://bafyFoo.com');
+    expect(url.protocol).to.equal('ftp:');
+    expect(url.pathname).to.equal('/');
+    expect(url.host).to.equal('bafyfoo.com');
+    expect(url.hostname).to.equal('bafyfoo.com');
   });
 });
