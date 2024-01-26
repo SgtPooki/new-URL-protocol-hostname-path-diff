70b44d56070dac81c59e3b810649c7d5bbb01cef
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-01-26 14:13:41 -0800

    chore: try to make test validation easier

--- test/URL.spec.js
+++ test/URL.spec.js
@@ -1,64 +1,206 @@
 import { expect } from 'aegir/chai';
 
-describe('URL-test', () => {
-  it(`ipfs:// URLs work in ${process.env.RUNNER_ENV}`, () => {
-    const url = new URL('ipfs://bafyFoo');
-    if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
-      expect(url.protocol).to.equal('ipfs:');
-      expect(url.pathname).to.equal('');
-      expect(url.host).to.equal('bafyFoo');
-      expect(url.hostname).to.equal('bafyFoo');
-    } else if (process.env.RUNNER_ENV.includes('chrome')) {
-      expect(url.protocol).to.equal('ipfs:');
-      expect(url.pathname).to.equal('//bafyFoo');
-      expect(url.host).to.equal('');
-      expect(url.hostname).to.equal('');
-    } else if (process.env.RUNNER_ENV.includes('firefox')) {
-      expect(url.protocol).to.equal('ipfs:');
-      expect(url.pathname).to.equal('/');
-      expect(url.host).to.equal('bafyfoo');
-      expect(url.hostname).to.equal('bafyfoo');
-    }
-  });
-  it(`ipns:// URLs work in ${process.env.RUNNER_ENV}`, () => {
-    const url = new URL('ipns://bafyFoo');
-    if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
-      expect(url.protocol).to.equal('ipns:');
-      expect(url.pathname).to.equal('');
-      expect(url.host).to.equal('bafyFoo');
-      expect(url.hostname).to.equal('bafyFoo');
-    } else if (process.env.RUNNER_ENV.includes('chrome')) {
-      expect(url.protocol).to.equal('ipns:');
-      expect(url.pathname).to.equal('//bafyFoo');
-      expect(url.host).to.equal('');
-      expect(url.hostname).to.equal('');
-    } else if (process.env.RUNNER_ENV.includes('firefox')) {
-      expect(url.protocol).to.equal('ipns:');
-      expect(url.pathname).to.equal('/');
-      expect(url.host).to.equal('bafyfoo');
-      expect(url.hostname).to.equal('bafyfoo');
-    }
-  });
+const getUrlString = (protocol, host, rest = '') => {
+  return `${protocol}://${host}${rest}`;
+}
 
-  it(`http:// URLs work in ${process.env.RUNNER_ENV}`, () => {
-    const url = new URL('http://bafyFoo.com');
-    expect(url.protocol).to.equal('http:');
-    expect(url.pathname).to.equal('/');
-    expect(url.host).to.equal('bafyfoo.com');
-    expect(url.hostname).to.equal('bafyfoo.com');
-  });
-  it(`https:// URLs work in ${process.env.RUNNER_ENV}`, () => {
-    const url = new URL('https://bafyFoo.com');
-    expect(url.protocol).to.equal('https:');
-    expect(url.pathname).to.equal('/');
-    expect(url.host).to.equal('bafyfoo.com');
-    expect(url.hostname).to.equal('bafyfoo.com');
-  });
-  it(`ftp:// URLs work in ${process.env.RUNNER_ENV}`, () => {
-    const url = new URL('ftp://bafyFoo.com');
-    expect(url.protocol).to.equal('ftp:');
-    expect(url.pathname).to.equal('/');
-    expect(url.host).to.equal('bafyfoo.com');
-    expect(url.hostname).to.equal('bafyfoo.com');
-  });
+const testTuples = [
+  ['ipfs', 'bafyFoo', ''],
+  ['ipfs', 'bafyFoo', '/path/file?query&query2#hash'],
+]
+
+function validateObjects (actual, expected) {
+  for (const key of Object.keys(expected)) {
+    expect(actual[key]).to.equal(expected[key], `Invalid value for \`url.${key}\` for "${actual.href}"`);
+  }
+}
+
+describe('URL-test', () => {
+  describe('ipfs:// URLs', () => {
+    let urlString = getUrlString('ipfs', 'bafyFoo');
+    it(`parses ipfs://bafyFoo in ${process.env.RUNNER_ENV}`, () => {
+      const urlString = getUrlString('ipfs', 'bafyFoo');
+      const url = new URL(urlString);
+      if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
+        validateObjects(url, {
+          protocol: 'ipfs:',
+          host: 'bafyFoo',
+          port: '',
+          hostname: 'bafyFoo',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '',
+          href: urlString
+        })
+      } else if (process.env.RUNNER_ENV.includes('chrome')) {
+        validateObjects(url, {
+          protocol: 'ipfs:',
+          host: '',
+          port: '',
+          hostname: '',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '//bafyFoo',
+          href: urlString
+        })
+      } else if (process.env.RUNNER_ENV.includes('firefox')) {
+        validateObjects(url, {
+          protocol: 'ipfs:',
+          host: 'bafyfoo',
+          port: '',
+          hostname: 'bafyfoo',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '/',
+          href: urlString.replace('bafyFoo', 'bafyfoo') + '/'
+        })
+      }
+    });
+
+    it(`parses ipfs://bafyFoo/path/file?myQueryK1=123&myQueryK2=456#myHashValue1=123&myHashValue2=456 in ${process.env.RUNNER_ENV}`, () => {
+      urlString = getUrlString('ipfs', 'bafyFoo', '/path/file?myQueryK1=123&myQueryK2=456#myHashValue1=123&myHashValue2=456')
+      const url = new URL(urlString);
+      if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
+        validateObjects(url, {
+          protocol: 'ipfs:',
+          host: 'bafyFoo',
+          port: '',
+          hostname: 'bafyFoo',
+          hash: '#myHashValue1=123&myHashValue2=456',
+          search: '?myQueryK1=123&myQueryK2=456',
+          query: undefined,
+          pathname: '/path/file',
+          href: urlString
+        })
+      } else if (process.env.RUNNER_ENV.includes('chrome')) {
+        validateObjects(url, {
+          protocol: 'ipfs:',
+          host: '',
+          port: '',
+          hostname: '',
+          hash: '#myHashValue1=123&myHashValue2=456',
+          search: '?myQueryK1=123&myQueryK2=456',
+          query: undefined,
+          pathname: '//bafyFoo/path/file',
+          href: urlString
+        })
+      } else if (process.env.RUNNER_ENV.includes('firefox')) {
+        validateObjects(url, {
+          protocol: 'ipfs:',
+          host: 'bafyfoo',
+          port: '',
+          hostname: 'bafyfoo',
+          hash: '#myHashValue1=123&myHashValue2=456',
+          search: '?myQueryK1=123&myQueryK2=456',
+          query: undefined,
+          pathname: '/path/file',
+          href: urlString.replace('bafyFoo', 'bafyfoo')
+        })
+      }
+    });
+  })
+  describe('ipns:// URLs', () => {
+    const protocol = 'ipns';
+    const urlString = getUrlString(protocol, 'bafyFoo');
+    it(`parses ${urlString} in ${process.env.RUNNER_ENV}`, () => {
+      const url = new URL(urlString);
+      if (['node', 'electron-main'].includes(process.env.RUNNER_ENV)) {
+        validateObjects(url, {
+          protocol: 'ipns:',
+          host: 'bafyFoo',
+          port: '',
+          hostname: 'bafyFoo',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '',
+          href: urlString
+        })
+      } else if (process.env.RUNNER_ENV.includes('chrome')) {
+        validateObjects(url, {
+          protocol: 'ipns:',
+          host: '',
+          port: '',
+          hostname: '',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '//bafyFoo',
+          href: urlString
+        })
+      } else if (process.env.RUNNER_ENV.includes('firefox')) {
+        validateObjects(url, {
+          protocol: 'ipns:',
+          host: 'bafyfoo',
+          port: '',
+          hostname: 'bafyfoo',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '/',
+          href: urlString.replace('bafyFoo', 'bafyfoo') + '/'
+        })
+      }
+    });
+  })
+
+  describe('http:// URLs', () => {
+    const protocol = 'http';
+    const urlString = getUrlString(protocol, 'bafyFoo');
+    it(`parses ${urlString} URLs work in ${process.env.RUNNER_ENV}`, () => {
+      const url = new URL(urlString);
+        validateObjects(url, {
+          protocol: 'http:',
+          host: 'bafyfoo',
+          port: '',
+          hostname: 'bafyfoo',
+          hash: '',
+          search: '',
+          query: undefined,
+          pathname: '/',
+          href: urlString.replace('bafyFoo', 'bafyfoo') + '/'
+        })
+    })
+  })
+
+  describe('https:// URLs', () => {
+    const protocol = 'https';
+    const urlString = getUrlString(protocol, 'bafyFoo');
+    it(`parses ${urlString} in ${process.env.RUNNER_ENV}`, () => {
+      const url = new URL(urlString);
+      validateObjects(url, {
+        protocol: 'https:',
+        host: 'bafyfoo',
+        port: '',
+        hostname: 'bafyfoo',
+        hash: '',
+        search: '',
+        query: undefined,
+        pathname: '/',
+        href: urlString.replace('bafyFoo', 'bafyfoo') + '/'
+      })
+    })
+  })
+
+  describe('ftp:// URLs', () => {
+    const protocol = 'ftp'
+    const urlString = getUrlString(protocol, 'bafyFoo');
+    it(`parses ${urlString} in ${process.env.RUNNER_ENV}`, () => {
+      const url = new URL(urlString);
+      validateObjects(url, {
+        protocol: 'ftp:',
+        host: 'bafyfoo',
+        port: '',
+        hostname: 'bafyfoo',
+        hash: '',
+        search: '',
+        query: undefined,
+        pathname: '/',
+        href: urlString.replace('bafyFoo', 'bafyfoo') + '/'
+      })
+    })
+  })
 });
